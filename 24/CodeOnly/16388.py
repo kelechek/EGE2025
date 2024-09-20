91d015c6475a00749e54e18ed4a627b520ed1f67
boris <boris@LAPTOP-655K9220>
2024-09-20 14:43:12 +0200

    Задание 24. №16388
    Источник: https://kompege.ru/

---
+++ 24/CodeOnly/16388.py
@@ -0,0 +1,16 @@
+with open('24_16388.txt') as file:
+    data = file.readline()
+
+data = data.replace('KLMN', '****')
+data = data.replace('*KLM', '**** ')
+data = data.replace('*KL', '*** ')
+data = data.replace('*K', '** ')
+data = data.replace('LMN*', ' ****')
+data = data.replace('MN*', ' ***')
+data = data.replace('N&', ' **')
+for i in 'KLMN':
+    data = data.replace(i, ' ')
+
+data = data.split()
+
+print(len(max(data, key=len)))
